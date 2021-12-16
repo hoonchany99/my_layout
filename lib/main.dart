bd52805682b252e6b0645cf62d3766e96be36ab1
윤훈찬 <hoonchan@yunhunchan-ui-MacBookPro.local>
2021-12-17 04:30:24 +0900

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'titleSection.dart';
+import 'buttonSection.dart';
+import 'textSection.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Widget t = const titleSection();
+    Widget b = const buttonSection();
+    Widget txt = const textSection();
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter layout demo'),
+          ),
+          body: ListView(
+            children: [t, b, txt],
+          )),
+    );
+  }
+}
